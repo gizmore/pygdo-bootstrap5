4cd27f1f2b9d0f4fe3bf987ad2f126c6de0739df
Christian Busch <cb@oncrea.de>
2025-02-01 21:06:56 +0100

    'bs5 sidebars'

--- js/gdo-bootstrap5.js
+++ js/gdo-bootstrap5.js
@@ -1,23 +1,4 @@
-const leftSidebar = document.getElementById('gdo-left-bar');
-const rightSidebar = document.getElementById('gdo-right-bar');
-const mainContent = document.getElementById('gdo-pagewrap');
-
-document.getElementById('toggleLeftSidebar').addEventListener('click', () => {
-    if (leftSidebar.style.display === 'block') {
-        leftSidebar.style.display = 'none';
-        mainContent.style.marginLeft = '0';
-    } else {
-        leftSidebar.style.display = 'block';
-        mainContent.style.marginLeft = '250px';
-    }
-});
-
-document.getElementById('toggleRightSidebar').addEventListener('click', () => {
-    if (rightSidebar.style.display === 'block') {
-        rightSidebar.style.display = 'none';
-        mainContent.style.marginRight = '0';
-    } else {
-        rightSidebar.style.display = 'block';
-        mainContent.style.marginRight = '250px';
-    }
-});
+window.GDO = window.GDO || {}
+window.GDO.toggleSidebar = function(side) {
+    document.getElementById(side).classList.toggle("collapsed");
+};
